f774467545b0415e8419c4a0363700cb4757eb74
cassdotcom <cassdotcom@users.noreply.github.com>
2015-06-13 16:54:06 +0100

    Create function-library.ps1

---
+++ rev_i/scripts/function-library.ps1
@@ -0,0 +1,3 @@
+Set-Alias get-fileName 'S:\TEST AREA\ac00418\reconversion\rev_i\scripts\get-filename.ps1'
+Set-Alias add-to-object 'S:\TEST AREA\ac00418\reconversion\rev_i\scripts\add-to-object.ps1'
+Set-Alias query-db  "S:\TEST AREA\ac00418\reconversion\rev_i\scripts\query-database.ps1"
